937921d208f5cfe6e4e4a9586942ffeb730ac33d
chickentuna <chickentuna@hotmail.com>
2013-05-08 11:24:58 +0200

    added tomato

--- EZ-Nosh/src/view/Window.java
+++ EZ-Nosh/src/view/Window.java
@@ -2,9 +2,12 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -15,6 +18,7 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -40,21 +44,25 @@ import events.RequestIngedientsEvent;
 import events.SuggestRecipesEvent;
 import javax.swing.border.LineBorder;
 import javax.swing.border.EmptyBorder;
-
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class Window {
 
 	/** Constants **/
-	private static final Border EMPTY_BORDER = BorderFactory.createEmptyBorder(0, 10, 10, 10);
-	private static final String L_LABEL_TITLE = "EZ-Nosh";
+	private static final String TITLE = "EZ-Nosh";
+	private static final Dimension FIELD_DIMENSION = new Dimension(100, 20);
+	private static final Dimension BOX_DIMENSION = new Dimension(20, 0);
+	private static final int FONT_SIZE = 18;
 
 	/** Components **/
 	private JFrame frame;
 	private ImagePanel panel;
-	
+
 	/** Control **/
-	private EventBus bus ;
-	
+	private EventBus bus;
+
 	public Window(EventBus bus) throws IOException {
 		this.bus = bus;
 		initComponents();
@@ -74,31 +82,45 @@ public class Window {
 			{
 				// SETUP cpane:top:center_panel:fc_panel:row0
 				JPanel row_title = new ImagePanel("title.png");
-				row_title.setOpaque(false);
-				row_title.setLayout(new BoxLayout(row_title, BoxLayout.X_AXIS));
-				row_title.setBorder(new EmptyBorder(0, 0, 0, 0));
-				{
-					JComponent row = row_title;
-				}
+				north_panel
+						.add(new Box.Filler(null, new Dimension(0, 10), null));
 				north_panel.add(row_title);
-
-				// SETUP cpane:top:center_panel:fc_panel:row1
-				JPanel row_filechooser = new JPanel();
-				row_filechooser.setOpaque(false);
-				row_filechooser.setLayout(new BoxLayout(row_filechooser,
-						BoxLayout.X_AXIS));
-				row_filechooser.setBorder(EMPTY_BORDER);
-				{
-					JComponent row = row_filechooser;
-				}
-				north_panel.add(row_filechooser);
+				north_panel
+						.add(new Box.Filler(null, new Dimension(0, 30), null));
 			}
 			panel.add(north_panel, BorderLayout.NORTH);
 
 			JPanel south_panel = new JPanel();
-			south_panel.setOpaque(false);
-			south_panel.setLayout(new BorderLayout());
+			{
+				south_panel.setLayout(new BorderLayout());
+				south_panel.setOpaque(false);
+
+				JPanel panel_1 = new JPanel();
+				panel_1.setAlignmentX(Component.LEFT_ALIGNMENT);
+				panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
+
+				addField(panel_1, "Nombres de repas à prévoir : ", 25);
+				panel_1.add(new Box.Filler(null, null, new Dimension(0, 10)));
+				addField(20, panel_1, "Rapide :");
+				addField(20, panel_1, "Picnic :");
+				addField(20, panel_1, "Chic :");
+				addField(20, panel_1, "Desserts :");
+
+				ImagePanel tomato = new ImagePanel("tomato.png");
+				tomato.setAlignmentX(Component.LEFT_ALIGNMENT);
+				panel_1.add(tomato);
+
+				panel_1.setOpaque(false);
+				south_panel.setAlignmentX(Component.LEFT_ALIGNMENT);
+				south_panel.add(panel_1, BorderLayout.CENTER);
+			}
 			panel.add(south_panel, BorderLayout.CENTER);
+			JPanel east_panel = new JPanel();
+			{
+			
+			}
+			panel.add(east_panel, BorderLayout.EAST);
+			
 		}
 
 		frame.setContentPane(panel);
@@ -106,14 +128,44 @@ public class Window {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
-		frame.setTitle("EZ-Nosh");
+		frame.setTitle(TITLE);
 
+	}
+
+	private void addField(int offset, JPanel panel_1, String string) {
+		addField(offset, panel_1, string, FONT_SIZE);
+
+	}
+
+	private void addField(JPanel panel_1, String string, int size) {
+		addField(6, panel_1, string, size);
+	}
+
+	private void addField(int offset, JPanel panel_1, String string, int size) {
+		JPanel panel_3 = new JPanel();
+		{
+			panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
+			panel_3.setOpaque(false);
+			JLabel label = new JLabel(string);
+			label.setFont(new Font("Serif", Font.PLAIN, size));
+			label.setAlignmentX(Component.LEFT_ALIGNMENT);
+			panel_3.add(new Box.Filler(null, null, new Dimension(offset, 0)));
+			panel_3.add(label);
+			panel_3.add(new Box.Filler(null, null, BOX_DIMENSION));
+			JTextField field = new JTextField("0");
+			field.setMaximumSize(FIELD_DIMENSION);
+			panel_3.add(field);
+		}
+		panel_3.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+		panel_1.add(panel_3);
+		panel_1.add(new Box.Filler(null, null, new Dimension(0, 10)));
 	}
 
 	private void initComponents() throws IOException {
 		frame = new javax.swing.JFrame();
 		panel = new ImagePanel("lgreen.jpg", ImagePanel.STRETCH);
-		
+
 	}
 
 	private void registerToEnter(JButton button, int condition) {
@@ -127,13 +179,13 @@ public class Window {
 
 	private void initActions() {
 	}
-	
+
 	@Subscribe
 	public void on(SuggestRecipesEvent e) {
 		System.out.println("Recipes suggested ");
 		bus.post(new RequestIngedientsEvent(e.getList()));
 	}
-	
+
 	@Subscribe
 	public void on(IngredientsGeneratedEvents e) {
 		String text = "<html>";
@@ -143,11 +195,11 @@ public class Window {
 			Pair<String, String> entry = it.next();
 			text = text + parseEntry(entry, a.get(entry)) + "<br>";
 		}
-		//label_test.setText(text+"</html>");
+		// label_test.setText(text+"</html>");
 	}
 
 	private String parseEntry(Pair<String, String> entry, Float value) {
-		return entry.getKey() + " : " + value + entry.getValue();  
+		return entry.getKey() + " : " + value + entry.getValue();
 	}
-	
+
 }
